504df3d2bb1df05e70fad94ab0e993b3db5e63d2
Kami Barut-Wanayo <kamibarut@yahoo.com>
2025-04-23 09:07:33 +0200

    Update PatientNotFoundException.php

--- Claim/Validation/Domain/Exceptions/PatientNotFoundException.php
+++ Claim/Validation/Domain/Exceptions/PatientNotFoundException.php
@@ -4,7 +4,7 @@ namespace Claim\Validation\Domain\Exceptions;
 
 use Exception;
 
-class PatientNotFoundExcecption extends Exception
+class PatientNotFoundException extends Exception
 {
     // Additional custom exception logic can be added here
 }
