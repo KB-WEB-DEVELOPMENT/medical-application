8c3e8c67c7ccaa8268093cc2165518f4830550ab
Kami Barut-Wanayo <kamibarut@yahoo.com>
2025-04-23 07:55:09 +0200

    Add files via upload

---
+++ Auth/Application/Http/Requests/LoginRequest.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+
+Custom request used to validate the user credentials and authenticate him / her.
+
+Used in file Auth\Application\Http\Controllers\LoginController.php
+
+See similar requests: 
+
+Claim\Submission\Application\Http\Requests\CreateClaimRequest.php
+Claim\Submission\Application\Http\Requests\UpdateClaimRequest.php
+
+*/
+   
