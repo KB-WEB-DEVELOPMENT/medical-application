912420d8caf8b6d1066cdfefd6fa95fd5c955901
Kami Barut-Wanayo <kamibarut@yahoo.com>
2025-04-23 09:06:43 +0200

    Add files via upload

---
+++ Claim/Validation/Domain/Exceptions/PatientNotFoundException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Claim\Validation\Domain\Exceptions;
+
+use Exception;
+
+class PatientNotFoundExcecption extends Exception
+{
+    // Additional custom exception logic can be added here
+}
