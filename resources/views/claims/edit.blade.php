59c4f9870a5555167f28075135a7f95ca0b7708b
Kami Barut-Wanayo <kamibarut@yahoo.com>
2025-04-23 09:30:24 +0200

    Add files via upload

---
+++ resources/views/claims/edit.blade.php
@@ -0,0 +1,8 @@
+<!--
+
+1. Template to be designed by the frontend developer
+2. Related controller: SubmissionClaimController
+3. Related controller method with available view data: edit(...)
+
+-->
+
---
+++ resources/views/claims/filtered_by_patient.blade.php
@@ -0,0 +1,7 @@
+<!--
+
+1. Template to be designed by the frontend developer
+2. Related controller: SubmissionClaimController
+3. Related controller method with available view data: byPatient(...)
+
+-->
